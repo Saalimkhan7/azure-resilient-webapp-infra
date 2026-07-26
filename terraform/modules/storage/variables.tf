a2809daba2e812072bb36cbe63247648ee6ed971
Saalimkhan7 <saalimk110@gmail.com>
2026-07-26 13:36:49 +0530

    Add Terraform module: storage (StorageV2, LRS, TLS 1.2, public access disabled)

---
+++ terraform/modules/storage/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Globally unique storage account name"
+  type        = string
+}
